2ac3c74ba556a203778cb778cbfb333bb4454d90
Stephen Lloyd <stephenlloyd@windermeresolutions.com>
2012-01-24 10:11:36 -0800

    Chapter 3 changes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,12 +4,18 @@ class ProjectsController < ApplicationController
 	end
 	def new
 		@project = Project.new
-	end
+  end
+
 	def create
 		@project = Project.new(params[:project])
-		@project.save
-		flash[:notice] = "Project has been created."
-		redirect_to @project
+		if @project.save
+		  flash[:notice] = "Project has been created."
+		  redirect_to @project
+    else
+      flash[:alert] = "Project has not been created."
+      render :action => "new"
+    end
+
 	end
 	def show
 		@project = Project.find(params[:id])
