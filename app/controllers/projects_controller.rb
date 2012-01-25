fbff8e0370aa3aea84dff26fa00ae297fe6d4f88
Stephen Lloyd <stephenlloyd@windermeresolutions.com>
2012-01-24 19:08:59 -0800

    Chp4 - Added updating projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
 	def index
-
+    @projects = Project.all
 	end
 	def new
 		@project = Project.new
@@ -19,6 +19,18 @@ class ProjectsController < ApplicationController
 	end
 	def show
 		@project = Project.find(params[:id])
-	end
-
+  end
+  def edit
+    @project = Project.find(params[:id])
+  end
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been updated."
+      render :action => "edit"
+    end
+  end
 end
