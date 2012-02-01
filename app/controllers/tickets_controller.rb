0d9e9c774a5fa5a261ada83f9d5f53fa742af0f7
Stephen Lloyd <stephenlloyd@windermeresolutions.com>
2012-01-31 20:44:37 -0800

    End of Chapter 5

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -18,6 +18,23 @@ class TicketsController < ApplicationController
   def show
 
   end
+  def edit
+
+  end
+  def update
+    if @ticket.update_attributes(params[:ticket])
+      flash[:notice] = "Ticket has been updated."
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "Ticket has not been updated."
+      render :action => "edit"
+    end
+  end
+  def destroy
+    @ticket.destroy
+    flash[:notice] = "Ticket has been deleted."
+    redirect_to @project
+  end
   private
   def find_project
     @project = Project.find(params[:project_id])
