cfed13e7c6eca9103641922e9b5d2078efc08be5
Stephen Lloyd <stephenlloyd@windermeresolutions.com>
2012-02-05 21:19:11 -0800

    End of chapter 6

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,5 +1,16 @@
 Given /^there are the following users:$/ do |table|
   table.hashes.each do |attributes|
+    unconfirmed = attributes.delete("unconfirmed") == "true"
     @user = User.create!(attributes)
+    @user.confirm! unless unconfirmed
   end
+end
+
+Given /^I am signed in as them$/ do
+  step %Q{I am on the homepage}
+  step %Q{I follow "Sign in"}
+  step %Q{I fill in "Email" with "#{@user.email}"}
+  step %Q{I fill in "Password" with "password"}
+  step %Q{I press "Sign in"}
+  step %Q{I should see "Signed in successfully."}
 end
