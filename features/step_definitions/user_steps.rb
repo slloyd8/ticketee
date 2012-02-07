402e6f4972f15fa049ffea779e8e567e390697c5
Stephen Lloyd <stephenlloyd@windermeresolutions.com>
2012-02-06 22:10:27 -0800

    cup 7.3.1 Restrict Access to project actions to admins only

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,15 +2,18 @@ Given /^there are the following users:$/ do |table|
   table.hashes.each do |attributes|
     unconfirmed = attributes.delete("unconfirmed") == "true"
     @user = User.create!(attributes)
+    @user.update_attribute("admin", attributes["admin"] == "true")
     @user.confirm! unless unconfirmed
   end
 end
 
 Given /^I am signed in as them$/ do
-  step %Q{I am on the homepage}
-  step %Q{I follow "Sign in"}
-  step %Q{I fill in "Email" with "#{@user.email}"}
-  step %Q{I fill in "Password" with "password"}
-  step %Q{I press "Sign in"}
-  step %Q{I should see "Signed in successfully."}
+  steps(%Q{
+    Given I am on the homepage
+    When I follow "Sign in"
+    And I fill in "Email" with "#{@user.email}"
+    And I fill in "Password" with "password"
+    And I press "Sign in"
+    Then I should see "Signed in successfully."
+  })
 end
